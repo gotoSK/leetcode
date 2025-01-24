f8fd14eb98357508bbf0d0fc0ca7d64ea008fc1d
unknown <karkisugam34@gmail.com>
2025-01-24 08:49:21 +0545

    attempt-1

---
+++ valid_sudoku.py
@@ -0,0 +1,47 @@
+import sys
+
+def isValidSudoku(board: list, check_type: str) -> bool:
+    if check_type == "sub-box":
+        availables = "123456789"
+    
+    for i in range(9):
+        
+        if check_type != "sub-box":
+            availables = "123456789"
+        
+        for j in range(9):
+            if check_type != "col": val = board[i][j]
+            else: val = board[j][i]
+
+            if val != ".":
+                valid = False
+                for x in availables:
+                    if val == x:
+                        valid = True
+                        availables = availables.replace(val, "")
+                        break
+                if not valid:
+                    return False
+    
+    return True
+
+
+if __name__ == "__main__":
+    board = [
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+    ]
+    
+    if isValidSudoku(board, "row"):
+        if isValidSudoku(board, "col"):
+            if isValidSudoku(board, "sub-box"):
+                print("Valid")
+                sys.exit()
+    print("Invalid")
