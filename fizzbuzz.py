531b2907522659840e98b32f8961d4dcaa6ea1fb
unknown <karkisugam34@gmail.com>
2025-01-25 14:07:20 +0545

    attempt-1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -15,3 +15,19 @@
 
 # Input: n = 5
 # Output: ["1", "2", "Fizz", "4", "Buzz"]
+
+def fizzBuzz(n: int) -> list:
+    answer = []
+    for i in range(1, n+1):
+        if i%3 == 0 and i%5 == 0:
+            answer.append("FizzBuzz")
+        elif i%3 == 0:
+            answer.append("Fizz")
+        elif i%5 == 0:
+            answer.append("Buzz")
+        else:
+            answer.append(str(i))
+    return answer
+
+if __name__ == "__main__":
+    print(fizzBuzz(int(input())))
